e9e312c5a3842d7b3b2fa72c154b50824729145f
PavelSharko <pushkin.94.72@gmail.com>
2024-09-19 17:13:41 +0700

    Second

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/hello-world")
+   @GetMapping("/")
     public String sayHello() {
         return "Hello World 3232";
     }
