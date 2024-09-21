a73ac074efdb494597557c686731caea8c8775c5
greaterr <greaterr@student.21-school.ru>
2024-09-21 23:51:34 +0500

    mapping_out

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/api")
 public class HelloController {
 
-   @GetMapping("/")
+  // @GetMapping("/hellowworld")
     public String sayHello() {
         return "Hello World";
     }
