f33b4cdc7b708abbe948264045671e022fb7df27
greaterr <greaterr@student.21-school.ru>
2024-10-01 00:17:51 +0500

    4th

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
 public class HelloWorldApplication extends SpringBootServletInitializer {
 
